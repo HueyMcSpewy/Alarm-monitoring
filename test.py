ad2cd28676d6861804120a25467196c84c422276
HueyMcSpewy <hueymcspewy0@gmail.com>
2026-03-04 22:30:57 +1100

    Update GPIO input handling in test.py
    Refactor GPIO input checking to use a loop and a different pin.

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import RPi.GPIO as GPIO
+import time
 
-test = GPIO.input(19)
+testingpinbcm= 4
 
-if test == 1:
-    print("nc")
-else:
-    print("no")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(testingpinbcm, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+while True:
+    test = GPIO.input(testingpinbcm)
+    if test == 1:
+        print("nc")
+    else:
+        print("no")
+    time.sleep(0.3)
